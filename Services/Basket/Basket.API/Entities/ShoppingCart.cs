6d7e149eb1ee4a230a45e7e6fdf46ef8fe1f3fcd
longden <nhokimlien1411@gmail.com>
2022-08-01 16:20:32 +0700

    basket microservices developed

---
+++ Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string _username)
+        {
+            this.username = _username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Quantity * item.Price;
+                }
+
+                return totalPrice;
+            }
+        }
+
+    }
+}
